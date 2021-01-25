d307bb97710ca439e7719210409350292d639046
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-25 23:50:49 +0000

    extend/include slugifiable in Sport
    - added Slugifiable methods to Sport

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,4 +3,8 @@ class Sport < ActiveRecord::Base
     has_many :racquets
     has_many :matches
     has_many :coaching_sessions
+
+    require_relative "./concerns/slugifiable"
+    extend Slugifiable::ClassMethods
+    include Slugifiable::InstanceMethods
 end
