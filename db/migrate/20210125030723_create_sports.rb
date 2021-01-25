6f87cb99e7ef294656f74e91350189e6ac09fc6e
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-25 05:38:29 +0000

    correct sports migration
    - correct column type for foreign key

--- db/migrate/20210125030723_create_sports.rb
+++ db/migrate/20210125030723_create_sports.rb
@@ -2,7 +2,7 @@ class CreateSports < ActiveRecord::Migration[6.0]
   def change
     create_table :sports do |t|
       t.string :name
-      t.string :user_id
+      t.integer :user_id
     end
   end
 end
