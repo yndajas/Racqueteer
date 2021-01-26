1dc55e5fbac1aab0b86b28649c829e42b961c302
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-26 05:38:25 +0000

    add extra slug method
    - added extra class method in Slugifiable module to find instance by slug and user ID

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -9,5 +9,9 @@ module Slugifiable
         def find_by_slug(slug)
             self.all.find { |instance| instance.slug == slug.downcase }
         end
+
+        def find_by_slug_and_id(slug, id)
+            self.where(user_id: id).find_by_slug(slug)
+        end
     end
 end
