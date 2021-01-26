00ff8671e79e54521f0d1284b993801525ce11cc
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-26 09:17:51 +0000

    change block spacing
    - added space between '{' and '|' in single-line blocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,39 +13,39 @@ class User < ActiveRecord::Base
     has_many :string_models
 
     def used_coaches
-        self.coaching_sessions.collect {|coaching_session| coaching_session.coaches }.flatten.uniq
+        self.coaching_sessions.collect { |coaching_session| coaching_session.coaches }.flatten.uniq
     end
 
     def used_frame_brands
-        self.used_racquets.collect {|racquet| racquet.frame_brand }.flatten.uniq
+        self.used_racquets.collect { |racquet| racquet.frame_brand }.flatten.uniq
     end
 
     def used_frame_models
-        self.used_racquets.collect {|racquet| racquet.frame_model }.flatten.uniq
+        self.used_racquets.collect { |racquet| racquet.frame_model }.flatten.uniq
     end
 
     def used_locations
-        (self.matches.collect {|match| match.location } + self.coaching_sessions.collect {|coaching_session| coaching_session.location }).flatten.uniq
+        (self.matches.collect { |match| match.location } + self.coaching_sessions.collect { |coaching_session| coaching_session.location }).flatten.uniq
     end
 
     def used_opponents
-        self.matches.collect {|match| match.opponent }.flatten.uniq
+        self.matches.collect { |match| match.opponent }.flatten.uniq
     end
 
     def used_racquets
-        self.matches.collect {|match| match.racquets }.flatten.uniq
+        self.matches.collect { |match| match.racquets }.flatten.uniq
     end
 
     def used_sports
-        (self.matches.collect {|match| match.sport } + self.coaching_sessions.collect {|coaching_session| coaching_session.sport }).flatten.uniq
+        (self.matches.collect { |match| match.sport } + self.coaching_sessions.collect { |coaching_session| coaching_session.sport }).flatten.uniq
     end
 
     def used_string_brands
-        self.used_racquets.collect {|racquet| racquet.string_brand }.flatten.uniq
+        self.used_racquets.collect { |racquet| racquet.string_brand }.flatten.uniq
     end
     
     def used_string_models
-        self.used_racquets.collect {|racquet| racquet.string_model }.flatten.uniq
+        self.used_racquets.collect { |racquet| racquet.string_model }.flatten.uniq
     end
 end
   
