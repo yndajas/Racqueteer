2f51e6ec321db6c1dccbd1b4e077346cbdab7de0
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-27 14:10:45 +0000

    add trim inputs script
    - added script to trim inputs on blur in order to avoid whitespace (alone) being submitted in a required field

---
+++ public/scripts/trim_inputs.js
@@ -0,0 +1,14 @@
+// after loading the page, add onblur event listener to any text or textarea inputs
+window.onload = function() {
+    var inputs = document.querySelectorAll('input[type=text],textarea');
+
+    for (let input of inputs) {
+        input.setAttribute("onblur", "trimValue(this)");
+    }
+}
+
+// trim element onblur
+function trimValue(element) {
+    console.log(element.value);
+    element.value = element.value.trim();
+}
