8c913d180dcfa0db68d134490ce0a57d42d42044
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-27 06:49:10 +0000

    update users controller
    - renamed post '/register' to post '/users'
    - added edit, update and destroy routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
-    # landing page when logged in
+    # show/landing page when logged in
     get '/dashboard' do
         erb :'/users/dashboard'
     end
 
+    # new
     get '/register' do
         redirect '/dashboard' if logged_in
         erb :'/users/register'
     end
 
-    post '/register' do
+    # create
+    post '/users' do
         # create user if they don't exist; set flash message based on whether they already existed
         if !User.find_by(email: params[:email])
             User.create(:email => params[:email], :password => params[:password])
@@ -19,6 +21,61 @@ class UsersController < ApplicationController
         end
         # redirect to log in page
         redirect '/login'
+    end
+
+    # edit
+    get '/account' do
+        erb :'/users/edit'
+    end
+
+    # update
+    patch '/users/:id' do
+        user = User.find(params[:id])
+        if user.authenticate(params[:password])
+            if params[:email]
+                user.update(email: params[:email])
+                flash[:message] = {:type => "success", :content => "Email successfully updated"}
+                redirect '/account'
+            else
+                user.update(password: params[:new_password])
+                flash[:message] = {:type => "success", :content => "Password successfully updated"}
+                redirect '/account'
+            end
+        else
+            flash[:message] = {:type => "danger", :content => "Incorrect password. Try again, unless you're a hacker!"}
+            redirect '/account'
+        end
+    end
+
+    # destroy
+    delete '/users/:id' do
+        user = User.find(params[:id])
+
+        CoachingSession.where(user_id: user.id).each do |coaching_session|
+            CoachingSessionCoach.where(coaching_session_id: coaching_session.id).destroy_all
+            coaching_session.destroy
+        end
+
+        Match.where(user_id: user.id).each do |match|
+            MatchRacquet.where(match_id: match.id).destroy_all
+            match.destroy
+        end
+
+        Coach.where(user_id: user.id).destroy_all
+        FrameBrand.where(user_id: user.id).destroy_all
+        FrameModel.where(user_id: user.id).destroy_all
+        Location.where(user_id: user.id).destroy_all
+        Opponent.where(user_id: user.id).destroy_all
+        Racquet.where(user_id: user.id).destroy_all
+        Sport.where(user_id: user.id).destroy_all
+        StringBrand.where(user_id: user.id).destroy_all
+        StringModel.where(user_id: user.id).destroy_all
+
+        user.destroy
+        session.clear
+
+        flash[:message] = {:type => "primary", :content => "Account successfully deleted"}
+        redirect '/'
     end
 
     get '/login' do
