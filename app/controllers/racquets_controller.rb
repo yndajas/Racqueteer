c2269a22ee965b435586b0d5d63703268205e972
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-27 06:45:47 +0000

    update racquets controller
    - abstracted code for getting and settings associates/attributes of racquets into helper methods
    - added edit, update and destroy routes

--- app/controllers/racquets_controller.rb
+++ app/controllers/racquets_controller.rb
@@ -1,11 +1,7 @@
 class RacquetsController < ApplicationController
     # new
     get '/racquets/new' do
-        @sports = Sport.where(user_id: current_user.id)
-        @frame_brands = FrameBrand.where(user_id: current_user.id)
-        @frame_models = FrameModel.where(user_id: current_user.id)
-        @string_brands = StringBrand.where(user_id: current_user.id)
-        @string_models = StringModel.where(user_id: current_user.id)
+        get_associates
         erb :'/racquets/new'
     end
 
@@ -13,11 +9,7 @@ class RacquetsController < ApplicationController
     post '/racquets' do
         racquet = Racquet.new
         racquet.user = current_user
-        racquet.sport = Sport.find_or_create_by(:name => params[:sport], :user_id => current_user.id)
-        racquet.frame_brand = FrameBrand.find_or_create_by(:name => params[:frame_brand], :user_id => current_user.id)
-        racquet.frame_model = FrameModel.find_or_create_by(:name => params[:frame_model], :user_id => current_user.id)
-        racquet.string_brand = StringBrand.find_or_create_by(:name => params[:string_brand], :user_id => current_user.id)
-        racquet.string_model = StringModel.find_or_create_by(:name => params[:string_model], :user_id => current_user.id)
+        set_associates(racquet)
         existing_racquet = Racquet.where(:frame_brand_id => racquet.frame_brand.id, :frame_model_id => racquet.frame_model.id, :string_brand_id => racquet.string_brand.id, :string_model_id => racquet.string_model.id, :sport_id => racquet.sport.id, :user_id => current_user.id)[0]
         if !existing_racquet
             racquet.save
@@ -54,6 +46,69 @@ class RacquetsController < ApplicationController
         else
             flash[:message] = {:type => "warning", :content => "Racquet not found"}
             redirect '/racquets'
+        end
+    end
+
+    # edit
+    get '/racquets/:id/edit' do
+        @racquet = Racquet.where(:id => params[:id], :user_id => current_user.id)[0]
+        if @racquet
+            get_associates
+            erb :'/racquets/edit'
+        else
+            flash[:message] = {:type => "warning", :content => "Racquet not found"}
+            redirect '/racquets'
+        end
+    end
+
+    # update
+    patch '/racquets/:id' do
+        racquet = Racquet.find(params[:id])
+        set_associates(racquet)
+        racquet.save
+        flash[:message] = {:type => "success", :content => "Racquet successfully updated"}
+        redirect "/racquets/#{racquet.id}"
+    end
+
+    # destroy
+    delete '/racquets/:id' do
+        racquet = Racquet.find(params[:id])
+
+        # arrays for storing the found matches and match racquets
+        match_racquets = []
+        matches = []
+        # get all match racquets belonging to the current racquet 
+        MatchRacquet.where(racquet_id: racquet.id).each do |match_racquet|
+            # add match in current match racquet to matches array
+            matches << Match.find(match_racquet.match_id)
+            # add all match racquets associated with match to match_racquets array (picking up match racquets that belong to other racquets but the same match, as well as current match racquet)
+            match_racquets << MatchRacquet.where(match_id: match_racquet.match_id)
+        end
+
+        # destroy collected records
+        match_racquets.flatten.uniq.each { |match_racquet| match_racquet.destroy }
+        matches.uniq.each { |match| match.destroy }
+        racquet.destroy
+
+        flash[:message] = {:type => "primary", :content => "Racquet successfully deleted"}
+        redirect "/racquets"
+    end
+    
+    helpers do
+        def get_associates
+            @sports = Sport.where(user_id: current_user.id)
+            @frame_brands = FrameBrand.where(user_id: current_user.id)
+            @frame_models = FrameModel.where(user_id: current_user.id)
+            @string_brands = StringBrand.where(user_id: current_user.id)
+            @string_models = StringModel.where(user_id: current_user.id)
+        end
+
+        def set_associates(racquet)
+            racquet.sport = Sport.find_or_create_by(:name => params[:sport], :user_id => current_user.id)
+            racquet.frame_brand = FrameBrand.find_or_create_by(:name => params[:frame_brand], :user_id => current_user.id)
+            racquet.frame_model = FrameModel.find_or_create_by(:name => params[:frame_model], :user_id => current_user.id)
+            racquet.string_brand = StringBrand.find_or_create_by(:name => params[:string_brand], :user_id => current_user.id)
+            racquet.string_model = StringModel.find_or_create_by(:name => params[:string_model], :user_id => current_user.id)    
         end
     end
 end
