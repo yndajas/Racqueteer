444b77996ff7161caf34fea95150cb95e9f4b708
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-28 15:35:00 +0000

    add racquet validation for matches
    - added JavaScript to check (1) all (or no) fields are complete for any new racquets (2) at least one racquet is selected

---
+++ public/scripts/validate_match_racquets.js
@@ -0,0 +1,52 @@
+function validateMatchRacquets() {
+    // get new racquet divs
+    var new_racquet_divs = document.getElementsByClassName("new_racquet");
+    if (validateNewRacquets(new_racquet_divs)) {
+        if (validateRacquetProvided(new_racquet_divs)) {
+            return true;
+        } else {
+            // if no racquet provided, send alert and return false
+            alert("Please select a racquet");
+            return false;
+        }
+    } else { // if partial information provided on a new racquet, send alert and return false
+        alert("Please complete all (or no) fields for new racquets");
+        return false;
+    }
+}
+
+function validateNewRacquets(new_racquet_divs) {
+    // iterate over new racquet inputs as many times as there are new racquet divs
+    for (let i = 0; i < new_racquet_divs.length; i++) {
+        // for each new racquet, check if any of its inputs have values
+        if (document.getElementById("frame_brand_" + (i + 1)).value.length > 0 || document.getElementById("frame_model_" + (i + 1)).value.length > 0 || document.getElementById("string_brand_" + (i + 1)).value.length > 0 || document.getElementById("string_model_" + (i + 1)).value.length > 0) {
+            // if they do, check if any of its inputs *don't* have values
+            if (!(document.getElementById("frame_brand_" + (i + 1)).value.length > 0 && document.getElementById("frame_model_" + (i + 1)).value.length > 0 && document.getElementById("string_brand_" + (i + 1)).value.length > 0 && document.getElementById("string_model_" + (i + 1)).value.length > 0)) {
+                // if so, return false
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function validateRacquetProvided(new_racquet_divs) {
+    // get existing racquet checkboxes
+    var existing_racquets = document.getElementsByClassName("existing_racquet_checkbox");
+    // iterate over existing racket checkboxes
+    for (let er of existing_racquets) {
+        // if checked, return true
+        if (er.checked) {
+            return true;
+        }
+    }
+    // iterate over frame_brands
+    for (let i = 0; i < new_racquet_divs.length; i++) {
+        // for each new racquet, check if the frame_brand has a value
+        if (document.getElementById("frame_brand_" + (i + 1)).value.length > 0) {
+            return true;
+        }
+    }    
+    // return false if incomplete racquet information
+    return false;
+}
