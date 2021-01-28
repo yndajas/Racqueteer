2d05a2b6189f542046d68f3e76554f740915b175
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-01-28 19:06:14 +0000

    add matches create route
    - added create route for matches

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,11 +6,42 @@ class MatchesController < ApplicationController
     end
 
     # create
-    # set_associates # sets sport, location and opponent
-    # iterate over racquets, creating a match_racquet for each
-    # iterate over new_racquets, creating a new racquet and match_racquet for each
-    # if params[:date], set start and end date to this
-    # else start_date = params[:start_date], end_date = params[:end_date]
+    post '/matches' do
+        match = Match.new
+        match.user = current_user
+        set_associates(match)
+        if params[:date]
+            match.start_date = params[:date]
+            match.end_date = params[:date]
+        else
+            match.start_date = params[:start_date]
+            match.end_date = params[:end_date]
+        end
+        match.score = params[:score]
+        match.result = params[:result]
+        match.save
+        # iterate over racquets, creating a match_racquet for each
+        if params[:racquet_ids]
+            params[:racquet_ids].each do |racquet_id|
+                MatchRacquet.create(:match_id => match.id, :racquet_id => racquet_id)
+            end
+        end
+        # iterate over new_racquets, creating a new racquet and match_racquet for each (if length of frame_brand > 0)
+        params[:new_racquets].each do |new_racquet|
+            if new_racquet[:frame_brand].length > 0
+                racquet = Racquet.find_or_create_by( \
+                    :frame_brand_id => FrameBrand.find_or_create_by(:name => new_racquet[:frame_brand], :user_id => current_user.id).id, \
+                    :frame_model_id => FrameModel.find_or_create_by(:name => new_racquet[:frame_model], :user_id => current_user.id).id, \
+                    :string_brand_id => StringBrand.find_or_create_by(:name => new_racquet[:string_brand], :user_id => current_user.id).id, \
+                    :string_model_id => StringModel.find_or_create_by(:name => new_racquet[:string_model], :user_id => current_user.id).id, \
+                    :sport_id => match.sport.id, :user_id => current_user.id)
+                MatchRacquet.find_or_create_by(:match_id => match.id, :racquet_id => racquet.id)
+            end
+        end
+        redirect "/matches/#{match.id}"
+    end
+
+    # create
 
     helpers do
         def get_associates
