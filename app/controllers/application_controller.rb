652c54d9759eee4144c67623df05aeed2ccf7840
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2021-02-03 19:40:52 +0000

    update current_user method
    - add instance variable and ||= to current user method to avoid querying the database multiple times within one route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
 
     # get current user instance
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id])
     end
 
     def get_ordered_racquets
